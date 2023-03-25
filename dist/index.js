c2f7925a8dadebcad545e59b673d8286042783f8
Wesley <69622989+Wesleyyy0808@users.noreply.github.com>
2023-03-25 21:43:34 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -8763,7 +8763,8 @@
         // if (ReposMap[project_name].type === 'isComponent') {
             // const { device } = ReposMap[project_name];
             // const data = await getContributorsFromWuji(device);
-            const data = await getContributorsFromWuji();
+            // const data = await getContributorsFromWuji();\
+            const data = JSON.parse('[{\"name\":\"QD\",\"fullName\":\"前端\",\"tasks\":[{\"name\":\"all\",\"fullName\":\"前端全部项目\",\"contributors\":[\"wesleyyy\"]}]},{\"name\":\"HD\",\"fullName\":\"后端\",\"tasks\":[{\"name\":\"all\",\"fullName\":\"后端全部项目\",\"contributors\":[\"ddos-ling\"]}]},{\"name\":\"issue\",\"fullName\":\"issue\",\"tasks\":[{\"name\":\"issue\",\"fullName\":\"issue\",\"contributors\":[\"wesleyyy\",\"ddos-ling\"]}]},{\"name\":\"bug\",\"fullName\":\"bug\",\"tasks\":[{\"name\":\"bug\",\"fullName\":\"bug\",\"contributors\":[\"wesleyyy\",\"ddos-ling\"]}]},{\"name\":\"PR\",\"fullName\":\"pr\",\"tasks\":[{\"name\":\"pr\",\"fullName\":\"pr\",\"contributors\":[\"wesleyyy\"]}]}]')
             //截取issue title中的[]的内容
             let component = issue_title.match(/\[(\w+)\]/i);
             if (!component) {
@@ -8779,12 +8780,11 @@
                 return ["wesleyyy"];
             }
     
+            // return componentData.contributors;
             return componentData.contributors;
-        }
-        return ["wesleyyy"];
+            // return ["wesleyyy"];
     // }
-    
-    
+
      (project_name, issue_title).then(data => {
         console.log(data);
         core.setOutput("contributors", data.join(','));
