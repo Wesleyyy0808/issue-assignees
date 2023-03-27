2fb1e48dfb938b2ed34787a59a5d233680e92a3c
Wesley <69622989+Wesleyyy0808@users.noreply.github.com>
2023-03-27 18:36:11 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -8777,7 +8777,7 @@
             //截取issue title中的[]的内容
             let component = issue_title.match(/\[(\w+)\]/i);
             if (!component) {
-                let component = issue_title.match(/\[([\u4e00-\u9fa5]+)\]/i);
+                component = issue_title.match(/\[([\u4e00-\u9fa5]+)\]/i);
                 if (!component) {
                     console.error('没有在issue的title中找到组件');
                     unmatch = 'true'
